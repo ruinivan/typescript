b694ad65490ef4855917736a984da6f5e0b24b24
Victor Ruinivan <victorruinivanvg@gmail.com>
2025-04-07 13:04:14 -0300

    training typescript

---
+++ exercise/frontend/build/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let num1;
+let coletar = prompt("Digite um numero: ");
+num1 = parseFloat(coletar);
+alert(typeof num1);
---
+++ exercise/frontend/src/script.ts
@@ -0,0 +1,4 @@
+let num1: number;
+let coletar: any = prompt("Digite um numero: ");
+num1 = parseFloat(coletar);
+alert(typeof num1);
